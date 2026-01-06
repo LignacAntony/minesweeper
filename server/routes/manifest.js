ba1cb644aa74233154bc353b280f03c49d25f97b
Anto <lignac.antony@live.fr>
2026-01-06 23:48:20 +0100

    Add accountAssociation to manifest

--- server/routes/manifest.js
+++ server/routes/manifest.js
@@ -6,6 +6,11 @@ router.get('/farcaster.json', (req, res) => {
     const appUrl = process.env.APP_URL || `https://${req.get('host')}`;
 
     const manifest = {
+        accountAssociation: {
+            header: "eyJmaWQiOjE0ODEzODcsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhFMzM1YzcxRTE3NkVhN0I0RjQxRjE3MUIzMWZEMDVmNDE5MDg3NjUyIn0",
+            payload: "eyJkb21haW4iOiJtaW5lc3dlZXBlci1hbHBoYS10aHJlZS52ZXJjZWwuYXBwIn0",
+            signature: "7SDFKKpAlgYQMc3AuaVgQI0XcOHN0LCKKy3jRRp9NF4vJ0kh4rDE6mKk14PUAPKWHagx9cK+wcx/5eeDD8Ie9Rs="
+        },
         frame: {
             name: "Minesweeper",
             version: "1",
